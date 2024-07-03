ce98f2ca4503d1b79abd4fc6c52dc470a68e5285
Hassaan Ahmad <62022834+hassaandev5@users.noreply.github.com>
2024-07-04 00:00:09 +0500

    modified: 11-todo-list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -2,6 +2,7 @@ const todoList = [];
 
 function addTodo() {
   const inputElement = document.querySelector(".js-todo-input");
+  const paraElement = document.querySelector(".js-todo-list");
   const todoName = inputElement.value;
 
   todoList.push(todoName);
