b199ecc225dab45868e2974488a21c6071bf8273
Hassaan Ahmad <62022834+hassaandev5@users.noreply.github.com>
2024-07-10 22:54:07 +0500

    modified: scripts/11-todo-list.js for adding styles

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -14,13 +14,12 @@ function renderTodoList() {
     const { name, dueDate } = todoObject;
 
     document.querySelector(".js-todo-list").innerHTML += `
-    <p>
-    ${name}  ${dueDate} 
+    <div>${name}</div>
+    <div>${dueDate}</div>
     <button onclick="
     todoList.splice(${i}, 1);
     renderTodoList();
-    ">Delete</button>
-    </p>`;
+    " class="delete-todo-button">Delete</button>`;
   }
   // console.log(document.querySelector(".js-todo-list"));
 }
